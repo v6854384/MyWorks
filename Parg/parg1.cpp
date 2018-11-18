ea65966ddbaa6de41ef4fb7c36792ab2f519acdd
Viktor <32385043+v6854384@users.noreply.github.com>
2018-11-19 02:55:35 +0300

    Delete parg1.cpp

--- Parg/parg1.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-int main (int argc,char*argv [])
-{
-   cout « "count=" <<argc-1<< endl;
-   for (int i=1 ; i<argc; i++)
-   {
-      cout << "argv ["<<i<<"]=" << argv [i] << endl;
-   }
-}
